062376a38b7167009e65f2e6df738a240ebc947e
yulinjie <yulinjie@example.com>
2025-03-23 20:56:00 +0800

    Update Google Analytics implementation

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,47 +1,25 @@
 'use client';
 
 import Script from 'next/script'
-import { useEffect } from 'react'
-
-declare global {
-  interface Window {
-    dataLayer: any[];
-  }
-}
 
 export default function Analytics() {
-  useEffect(() => {
-    // 确保在客户端执行
-    if (typeof window !== 'undefined') {
-      window.dataLayer = window.dataLayer || [];
-      function gtag(...args: any[]) {
-        window.dataLayer.push(arguments);
-      }
-      gtag('js', new Date());
-      gtag('config', 'G-CT9DWHWHF7');
-    }
-  }, []);
-
   return (
     <>
       <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-CT9DWHWHF7`}
+        src="https://www.googletagmanager.com/gtag/js?id=G-CT9DWHWHF7"
+        strategy="beforeInteractive"
       />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-CT9DWHWHF7', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      <Script id="google-analytics" strategy="beforeInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-CT9DWHWHF7', {
+            send_page_view: true,
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
     </>
   )
 } 
