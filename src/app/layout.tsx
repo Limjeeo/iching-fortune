265149c97f9fa38f625cbfd86d161427b449c1f2
yulinjie <yulinjie@example.com>
2025-03-23 20:56:46 +0800

    Add Google Analytics to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,11 +12,27 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="zh">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-CT9DWHWHF7"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CT9DWHWHF7');
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.className} bg-black min-h-screen text-white`}>
         <Analytics />
         {children}
