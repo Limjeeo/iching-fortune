4ed3b2eae9e8ce1f93050a3d2598daaaf8c5c825
yulinjie <yulinjie@example.com>
2025-03-23 20:20:38 +0800

    Update config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/iching-fortune',
   images: {
     unoptimized: true,
-  },
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true
   }
 }
 
