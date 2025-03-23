d825a68fb16d8b37552b199b6538fa4f9719088c
yulinjie <yulinjie@example.com>
2025-03-23 20:21:56 +0800

    Remove basePath for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/iching-fortune',
   images: {
     unoptimized: true,
-  },
+  }
 }
 
 module.exports = nextConfig 
